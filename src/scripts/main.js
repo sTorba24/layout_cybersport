8a5e70510cd289a69b590526c9ea5297b4abf8d4
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-11-05 23:49:30 +0200

    soluton with adaptive bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,6 @@ document.querySelectorAll('.header__toggler').forEach(function(indicator, index)
     indicator.classList.add('selected');
 
     console.log(index);
-
     if(index === 0) {
       slider.classList.add('first');
       slider.classList.remove('second');
@@ -24,3 +23,61 @@ document.querySelectorAll('.header__toggler').forEach(function(indicator, index)
     }
   });
 });
+
+document.querySelectorAll('.main__toggler').forEach(function(indicator, index) {
+  indicator.addEventListener('click', function() {
+    document.querySelector('.selected__main__toggler').classList.remove('selected__main__toggler');
+    indicator.classList.add('selected__main__toggler');
+  
+    if(index === 0) {
+      document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+      document.querySelector('.matches--container__first').classList.add('matches--container__visible');
+    } else if(index === 1) {
+      document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+      document.querySelector('.matches--container__second').classList.add('matches--container__visible');
+    } else {
+      document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+      document.querySelector('.matches--container__third').classList.add('matches--container__visible');
+    }
+  });
+});
+
+const f = () => {
+  document.querySelectorAll('.main__toggler').forEach(function(indicator, index) {
+    indicator.addEventListener('click', function() {
+      document.querySelector('.selected__main__toggler').classList.remove('selected__main__toggler');
+      indicator.classList.add('selected__main__toggler');
+    
+      if(index === 0) {
+        document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+        document.querySelector('.matches--container__first').classList.add('matches--container__visible');
+      } else if(index === 1) {
+        document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+        document.querySelector('.matches--container__second').classList.add('matches--container__visible');
+      } else {
+        document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+        document.querySelector('.matches--container__third').classList.add('matches--container__visible');
+      }
+    });
+  });
+}
+
+document.querySelectorAll('.page__main__button').forEach(function(indicator, index) {
+  indicator.addEventListener('click', function() {
+    document.querySelector('.page__main__button-selected').classList.remove('page__main__button-selected');
+    indicator.classList.add('page__main__button-selected');
+    document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+    document.querySelector('.shtuka').classList.add('matches--container__visible');
+    console.log(index);
+    if(index === 1) {
+      document.querySelectorAll('.main__toggler').forEach(function(indicator, index) {
+        indicator.parentElement.innerHTML += '';
+      });
+    } else {
+      document.querySelector('.matches--container__visible').classList.remove('matches--container__visible');
+      document.querySelector('.matches--container__first').classList.add('matches--container__visible');
+      f();
+    }
+  });
+});
+
